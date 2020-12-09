1ad898dea542f018b729b5b547d176f75cd1af0b
bit <bit@DESKTOP-LOS0TN6>
2020-12-09 12:13:05 +0900

    practice03

---
+++ practice03/src/main/java/prob2/Prob2.java
@@ -0,0 +1,16 @@
+package prob2;
+
+public class Prob2 {
+	public static void main(String[] args) {
+		/*코드추가*/
+		int[] units = {50000, 10000, 5000, 1000}; 
+		
+		int money = 67879;
+		
+		
+		int count50000 = 67879 / 50000;
+		money -= (count50000*50000);
+		
+	}
+}
+
---
+++ practice03/src/main/java/prob6/Depart.java
@@ -0,0 +1,8 @@
+package prob6;
+
+public class Depart extends Employee {
+	String department;
+
+	public Depart(String name, int salary, String department) {
+	}
+}
