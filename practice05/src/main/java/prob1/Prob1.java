ac6d1fbefa8a3a4aa6eb4a9a3df745ee149272da
bit <bit@DESKTOP-LOS0TN6>
2020-12-09 14:16:37 +0900

    practice05

---
+++ practice05/src/main/java/prob1/Prob1.java
@@ -0,0 +1,19 @@
+package prob1;
+import java.util.Scanner;
+
+
+public class Prob1 {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner( System.in );
+		System.out.print( "문자열을 입력하세요:" );
+		
+		String s = scanner.next();
+		
+		/*  이곳에 프로그램을 완성하십시오. */
+		for(int i = 0 ; i < s.length(); i++) {
+//			for(int j = 0; j <)
+			
+		}
+	}
+}
