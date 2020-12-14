6b30a66a90af57e577d05b150722df3ca27141af
javafullstack-bitacademy <javafullstack.bitacademy@gmail.com>
2020-12-14 15:31:27 +0900

    TCP Socket Programming 연습 #2

--- network/src/main/java/test/TCPServer.java
+++ network/src/main/java/test/TCPServer.java
@@ -1,8 +1,12 @@
 package test;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class TCPServer {
 
@@ -17,9 +21,47 @@ public class TCPServer {
 			serverSocket.bind(new InetSocketAddress("127.0.0.1", 6000));
 
 			//3. accept
-			serverSocket.accept();  // blocking
+			Socket socket = serverSocket.accept();  // blocking
 			
-			System.out.println("dqwdjqwjdqwkj");
+			InetSocketAddress remoteInetSocketAddress = (InetSocketAddress)socket.getRemoteSocketAddress();
+			InetAddress remoteInetAddress = remoteInetSocketAddress.getAddress();
+			String remoteHostAddress = remoteInetAddress.getHostAddress();
+			int remotePort = remoteInetSocketAddress.getPort();
+			System.out.println("[server] connected by client[" + remoteHostAddress + ":" + remotePort + "]");
+			
+			try {
+				//4. IOStream 받아 오기
+				InputStream is = socket.getInputStream();
+				OutputStream os = socket.getOutputStream();
+				
+				while(true) {
+					//5. 데이터 읽기
+					byte[] buffer = new byte[256];
+					int readByteCount = is.read(buffer); // blocking
+					
+					if(readByteCount == -1){
+						// client가 소켓을 정상적으로 종료
+						System.out.println("[server] closed by client");
+						break;
+					}
+					
+					String data = new String(buffer, 0, readByteCount, "UTF-8");
+					System.out.println("[server] received:" + data);
+					
+					//6. 데이터쓰기
+					os.write(data.getBytes("utf-8"));
+				}
+				
+				
+			} catch(IOException e) {
+				System.out.println("[server] error:" + e);
+			} finally {
+				try {
+					socket.close();
+				} catch(IOException e) {
+					e.printStackTrace();
+				}
+			}
 			
 		} catch (IOException e) {
 			System.out.println("[server] error:" + e);
