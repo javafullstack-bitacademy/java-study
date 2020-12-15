4e17bda9e9d32474f0d1ccf8e3af2aad6fed7842
javafullstack-bitacademy <javafullstack.bitacademy@gmail.com>
2020-12-15 14:49:42 +0900

    Web Server 작성 #completed

--- network/src/main/java/http/RequestHandler.java
+++ network/src/main/java/http/RequestHandler.java
@@ -1,13 +1,16 @@
 package http;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.nio.file.Files;
 
 public class RequestHandler extends Thread {
+	private static final String DOCUMENT_ROOT = "./webapp";
 	private Socket socket;
 	
 	public RequestHandler( Socket socket ) {
@@ -25,7 +28,8 @@ public class RequestHandler extends Thread {
 			// logging Remote Host IP Address & Port
 			InetSocketAddress inetSocketAddress = ( InetSocketAddress )socket.getRemoteSocketAddress();
 			consoleLog( "connected from " + inetSocketAddress.getAddress().getHostAddress() + ":" + inetSocketAddress.getPort() );
-			
+
+			String request = null;
 			while(true) {
 				String line = br.readLine();
 				
@@ -35,31 +39,69 @@ public class RequestHandler extends Thread {
 					break;
 				}
 				
-				consoleLog("received:" + line);				
+				// Request Header의 첫번째 라인만 읽음
+				if(request == null) {
+					request = line;
+					break;
+				}
 			}
 			
-			// 예제 응답입니다.
-			// 서버 시작과 테스트를 마친 후, 주석 처리 합니다.
-			outputStream.write( "HTTP/1.1 200 OK\r\n".getBytes( "UTF-8" ) );
-			outputStream.write( "Content-Type:text/html; charset=utf-8\r\n".getBytes( "UTF-8" ) );
-			outputStream.write( "\r\n".getBytes() );
-			outputStream.write( "<h1>Hello Chrome!!</h1>".getBytes( "UTF-8" ) );
+			consoleLog(request);
+			
+			String[] tokens = request.split(" ");
+			if("GET".equals(tokens[0])) {
+				responseStaticResource(outputStream, tokens[1], tokens[2]);
+			} else { // POST, DELETE, PUT, HEAD, CONNECT
+				
+				 // HTTP/1.1 400 Bad Request\r\n
+				 // Content-Type:text/html; charset=utf-8\r\n
+				 // \r\n
+				 // html 에러 문서(./webapp/error/400.html)
 
-		} catch( Exception ex ) {
-			consoleLog( "error:" + ex );
+				 //response400Error();
+			}
+
+		} catch(Exception ex) {
+			consoleLog("error:" + ex);
 		} finally {
 			// clean-up
 			try{
-				if( socket != null && socket.isClosed() == false ) {
+				if(socket != null && socket.isClosed() == false) {
 					socket.close();
 				}
-				
-			} catch( IOException ex ) {
+			} catch(IOException ex) {
 				consoleLog( "error:" + ex );
 			}
 		}			
 	}
 
+	public void responseStaticResource(OutputStream outputStream, String uri, String protocol) throws IOException {
+		if("/".equals(uri)) {
+			uri = "/index.html";
+		}
+		
+		File file = new File(DOCUMENT_ROOT + uri);
+		if(!file.exists()) {
+			 // HTTP/1.1 404 Not Found\r\n
+			 // Content-Type:text/html; charset=utf-8\r\n
+			 // \r\n
+			 // html 에러 문서(./webapp/error/404.html)
+			
+			// response404Error();
+			return;
+		}
+		
+		// nio
+		byte[] body = Files.readAllBytes(file.toPath());
+		String contentType = Files.probeContentType(file.toPath());
+		
+		// response
+		outputStream.write((protocol + " 200 OK\r\n").getBytes( "UTF-8" ));
+		outputStream.write(("Content-Type:" + contentType + "; charset=utf-8\r\n").getBytes("UTF-8"));
+		outputStream.write("\r\n".getBytes("UTF-8"));
+		outputStream.write(body);
+	}
+	
 	public void consoleLog( String message ) {
 		System.out.println( "[RequestHandler#" + getId() + "] " + message );
 	}
